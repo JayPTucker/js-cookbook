e200c0957fd5eaacc4686bf892d8b05cfb3807c4
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-03-17 19:59:13 -0500

    Added more HTML Elements and Edited the Recipe.JS File with a new Ajax Function

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,17 +1,14 @@
-var input = "chicken"
 var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://webknox-recipes.p.rapidapi.com/recipes/search?query=burger",
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
-        "x-rapidapi-key": "8cf712f16dmsh2a112cebaf69ed6p1d6b2djsn121e0beeafed"
-    }
+	"async": true,
+	"crossDomain": true,
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=flour%252Ccinnamon",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		"x-rapidapi-key": "8cf712f16dmsh2a112cebaf69ed6p1d6b2djsn121e0beeafed"
+	}
 }
 
 $.ajax(settings).done(function (response) {
-
-    console.log(response);
-    
+	console.log(response);
 });
