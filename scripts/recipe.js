d8f00c0a4d495237ed891edbeca4d6d3ea4be223
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-03-17 19:28:43 -0500

    Created a Basic HTML Format

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,15 +1,17 @@
 var input = "chicken"
 var settings = {
-"async": true,
-"crossDomain": true,
-"url": "https://webknox-recipes.p.rapidapi.com/recipes/search?query=burger",
-"method": "GET",
-"headers": {
-    "x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
-    "x-rapidapi-key": "8cf712f16dmsh2a112cebaf69ed6p1d6b2djsn121e0beeafed"
-}
+    "async": true,
+    "crossDomain": true,
+    "url": "https://webknox-recipes.p.rapidapi.com/recipes/search?query=burger",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+        "x-rapidapi-key": "8cf712f16dmsh2a112cebaf69ed6p1d6b2djsn121e0beeafed"
+    }
 }
 
 $.ajax(settings).done(function (response) {
-console.log(response);
+
+    console.log(response);
+    
 });
