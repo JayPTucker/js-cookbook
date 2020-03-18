10f77d9f66dcbff715cef33d32bf93a6e2685f05
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-03-17 20:15:16 -0500

    Added Comments to help better understand the Javascript code

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,7 +1,26 @@
+// AJAX CALL FOR GETTING THE RECIPE ID
+
 var settings = {
 	"async": true,
 	"crossDomain": true,
 	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=flour%252Ccinnamon",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		"x-rapidapi-key": "8cf712f16dmsh2a112cebaf69ed6p1d6b2djsn121e0beeafed"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/1348203/information",
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
