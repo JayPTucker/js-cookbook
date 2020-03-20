dca80b443793c4ad9f4edda40f7320c53a56605c
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-03-19 19:40:13 -0500

    grabbed values needed from ajax calls and added input boxes

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,3 +1,49 @@
+
+// Array to hold the 10 grabbed ids from first api call
+
+var idArray = [];
+
+var inputArray = [];
+
+
+// Grab input box and button for finding recipes
+
+var searchInput = $("#search");
+
+var searchBtn   = $("#search-btn");
+
+var inputBox    = $("#input-box");
+
+var addBtn      = $("#add");
+
+// Add event listeners
+
+searchBtn.on("click", function(e) {
+
+	e.preventDefault();
+
+	var userInput = searchInput.val().trim().toLowerCase();
+	inputArray.push(userInput);
+
+	console.log(inputArray);
+
+});
+
+addBtn.on("click", function(e) {
+
+	e.preventDefault();
+
+	var newInput = $("<input>").attr("id", "search");
+	inputBox.append(newInput);
+
+})
+
+
+
+// Variables for searched ingredients
+
+var ingredients = "";
+
 // AJAX CALL FOR GETTING THE RECIPE ID
 
 var settings = {
@@ -9,46 +55,64 @@ var settings = {
 		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
 		"x-rapidapi-key": "8cf712f16dmsh2a112cebaf69ed6p1d6b2djsn121e0beeafed"
 	}
-}
+};
 
 $.ajax(settings).done(function (response) {
-	console.log(response);
-});
+	
 
+	for (var i = 0; i < 10; i++) {
 
-// AJAX CALL FOR GETTING THE RECIPE INFO BY THE ID
+		var id = response[i].id;
+		idArray.push(id);
 
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/1036801/information",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-		"x-rapidapi-key": "8cf712f16dmsh2a112cebaf69ed6p1d6b2djsn121e0beeafed"
-	}
-}
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-
-
-	// GRAB TITLE OF SELECTED DISH
-	console.log(response.title);
-
-	// GRAB PICTURE OF SELECTED DISH
-	console.log(response.image);
-
-	// GRAB DESCRIPTION OF SELECTED DISH
-	console.log(response.summary);
-
-	// GRAB INGREDIENTS OF SELECTED DISH
-	for ( var i = 0; i < response.extendedIngredients.length; i++) {
-	console.log(response.extendedIngredients[i].originalString);
 	};
 
-	// GRAB COOKING INSTRUCTIONS OF SELECTED DISH
-	console.log(response.instructions);
-
+	secondCall();
 
 });
+
+// AJAX CALL FOR GETTING THE RECIPE INFO BY THE ID
+function secondCall() {
+
+	for (var i = 0; i < idArray.length; i++) {
+
+		var idNum = idArray[i];
+
+		var settings = {
+			"async": true,
+			"crossDomain": true,
+			"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"+ idNum + "/information",
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+				"x-rapidapi-key": "8cf712f16dmsh2a112cebaf69ed6p1d6b2djsn121e0beeafed"
+			}
+		}
+
+		$.ajax(settings).done(function (response) {
+
+			// GRAB TITLE OF SELECTED DISH
+			console.log(response.title);
+
+			// GRAB PICTURE OF SELECTED DISH
+			console.log(response.image);
+
+			// GRAB DESCRIPTION OF SELECTED DISH
+			console.log(response.summary);
+
+			// GRAB INGREDIENTS OF SELECTED DISH
+			for ( var i = 0; i < response.extendedIngredients.length; i++) {
+
+			console.log(response.extendedIngredients[i].originalString);
+
+			};
+
+			// GRAB COOKING INSTRUCTIONS OF SELECTED DISH
+			console.log(response.instructions);
+
+
+		});
+
+	};
+
+};
